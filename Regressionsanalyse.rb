97daf4d7e7a59c90a89be93e5d4e23dd9bf3c6a5
Philipp Menge <pmenge@me.com>
2015-04-18 17:49:28 +0200

    prettify it the class integration

--- Regressionsanalyse.rb
+++ Regressionsanalyse.rb
@@ -1,38 +1,39 @@
 x = [2.9, 4.1, 4.9, 5.0, 4.4, 3.1, 3.9, 3.7]
 y = [6, 9, 8, 7, 6, 7, 8, 5]
 
-def quadrieren(array)
-  hurz = []
-  array.cycle(1) { |x| hurz << x*x }
-  hurz
-end
-
-def summe(array)
-  array.inject(:+)
-end
-
-def mittelwert(array)
-  summe(array) / array.length
-end
-
-def arrays_multiplizieren(array1,array2)
-  i = 0
-  ergebnis = []
-  if !array1.length == array2.length
-    puts 'unterschiedlich lange messreihen, du lappen'
-    return
-  end
-
-  array1.each_with_index do |x,i|
-    ergebnis << array1[i] * array2[i]
-  end
-  ergebnis
-end
-
 class Regression
   def initialize(x, y)
     @x = x
     @y = y
+
+
+    def quadrieren(array)
+      hurz = []
+      array.cycle(1) { |x| hurz << x*x }
+      hurz
+    end
+
+    def summe(array)
+      array.inject(:+)
+    end
+
+    def mittelwert(array)
+      summe(array) / array.length
+    end
+
+    def arrays_multiplizieren(array1,array2)
+      i = 0
+      ergebnis = []
+      if !array1.length == array2.length
+        puts 'unterschiedlich lange messreihen, du lappen'
+        return
+      end
+
+      array1.each_with_index do |x,i|
+        ergebnis << array1[i] * array2[i]
+      end
+      ergebnis
+    end
   end
 
   def lineare_regressionsanalyse
